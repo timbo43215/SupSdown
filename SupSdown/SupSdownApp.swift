a8a87223f7a138eb80e9e523940b05cf54fbe54c
Tim Stack <tstack43215@yahoo.com>
2023-02-03 13:16:47 -0600

    Initial Commit

---
+++ SupSdown/SupSdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupSdownApp.swift
+//  SupSdown
+//
+//  Created by IIT PHYS 440 on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SupSdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
